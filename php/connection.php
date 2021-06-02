364ef65b32d3761e2b4b705d8fb070abe5afd331
Nicolas Felipe Batista <69852261+Nicolas-FB@users.noreply.github.com>
2021-06-01 22:49:36 -0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,13 +1,16 @@
 <?php
 
-$dbhost = "ec2-54-243-92-68.compute-1.amazonaws.com
-";
-$dbuser = "erbtjbzxgnerkz";
-$dbpass = "e5b5e75974f65d29dbe357dfa17ac218c915b803b40cc97a9a7a1c96556ea438";
-$dbname = "d8pveiffejcoc7";
+   $con = "dbname=d8pveiffejcoc7 host=ec2-54-243-92-68.compute-1.amazonaws.com
+ port=5432 user=erbtjbzxgnerkz password=e5b5e75974f65d29dbe357dfa17ac218c915b803b40cc97a9a7a1c96556ea438 sslmode=require";
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
 
-	die("failed to connect!");
-}
+   if (!$con) 
+   {
+     echo "Database connection failed.";
+   }
+   else 
+   {
+     echo "Database connection success.";
+   }
+
+?>
