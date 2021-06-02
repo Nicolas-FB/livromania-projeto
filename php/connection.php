37afdd220817a34342ff8a01945021fde9956acf
Nicolas Felipe Batista <69852261+Nicolas-FB@users.noreply.github.com>
2021-06-01 21:16:35 -0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,9 +1,10 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "login_livromania";
+$dbhost = "ec2-54-243-92-68.compute-1.amazonaws.com
+";
+$dbuser = "erbtjbzxgnerkz";
+$dbpass = "e5b5e75974f65d29dbe357dfa17ac218c915b803b40cc97a9a7a1c96556ea438";
+$dbname = "d8pveiffejcoc7";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
