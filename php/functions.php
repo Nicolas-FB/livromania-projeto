b959834be582678179c7473285861002b1fc4e32
Nicolas Felipe Batista <69852261+Nicolas-FB@users.noreply.github.com>
2021-06-03 21:18:06 -0300

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -9,17 +9,17 @@ function check_login($con)
 		$id = $_SESSION['user_id'];
 		$query = "select * from users where user_id = '$id' limit 1";
 
-		$result = mysqli_query($con,$query);
-		if($result && mysqli_num_rows($result) > 0)
+		$result = pg_query($con,$query);
+		if($result && pg_num_rows($result) > 0)
 		{
 
-			$user_data = mysqli_fetch_assoc($result);
+			$user_data = pg_fetch_assoc($result);
 			return $user_data;
 		}
 	}
 
 	//redirect to login
-	header("Location: index.php");
+	header("Location: login.php");
 	die;
 
 }
